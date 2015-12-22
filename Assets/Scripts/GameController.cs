26ce59572d66581332aa6830d145de59cb90b0d9
oykunn <oykun.balkanay@gmail.com>
2015-12-22 22:05:54 +0200

    Base
    Downloads içindekiler düzeltilmeli ve anlaşılır hale getirilmeli

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+
+	void Start ()
+	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
+
+	}
+
+	void Update ()
+	{
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+		if (gameOver)
+		{
+			restartText.text = "Press 'R' for Restart";
+			restart = true;
+		}	
+	}
+
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
+}
---
+++ Assets/Scripts/Wave1Move.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave1Move : MonoBehaviour {
+
+	public Vector3 point1;
+	public Vector3 point2;
+	public Vector3 point3;
+	public Vector3 point4;
+	public Vector3 point5;
+	public Vector3 point6;
+	public Vector3 point7;
+	public Vector3 point8;
+	public Vector3 point9;
+	public Vector3 point10;
+	public Vector3 pointEnd;
+
+	IEnumerator Start () {
+		var pointStart = transform.position;
+		while (true){
+			yield return StartCoroutine (MoveObject (transform, pointStart, point1, 3.0f));
+			yield return StartCoroutine (MoveObject (transform, point1, point2, 3.0f));
+			yield return StartCoroutine (MoveObject (transform, point2, point3, 3.0f));
+			yield return StartCoroutine (MoveObject (transform, point3, point4, 3.0f));
+			yield return StartCoroutine (MoveObject (transform, point4, point5, 3.0f));
+			yield return StartCoroutine (MoveObject (transform, point5, point6, 3.0f));
+			yield return StartCoroutine (MoveObject (transform, point6, point7, 3.0f));
+			yield return StartCoroutine (MoveObject (transform, point7, point8, 3.0f));
+			yield return StartCoroutine (MoveObject (transform, point8, point9, 3.0f));
+			yield return StartCoroutine (MoveObject (transform, point9, point10, 3.0f));
+			yield return StartCoroutine (MoveObject (transform, point10, pointEnd, 3.0f));
+		}
+	}
+
+	IEnumerator MoveObject (Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+	{
+		var i = 0.0f;
+		var rate = 1.0f / time;
+		while (i<1.0f ){
+			i += Time.deltaTime * rate;
+			thisTransform.position = Vector3.Lerp (startPos, endPos, i);
+			yield return null;
+			}
+	}
+}
